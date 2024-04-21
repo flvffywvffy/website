88330b49e8e22763637cfb7a710e54ad8c9bd8d1
flvffywvffy <marcus@marcusleonasharvey.com>
2024-04-21 18:04:22 +0100

    Major changes

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,34 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
 });
 
-export const collections = { blog };
+const shows = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+
+    showDate: z.coerce.date(),
+
+    director: z.string(),
+    company: z.string(),
+
+    roles: z.array(z.string()),
+  }),
+});
+
+export const collections = {
+  blog: blog,
+  shows: shows,
+};
